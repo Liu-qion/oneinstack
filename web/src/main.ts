a6b35905f54de8b411326c5d06a701abbff8fb50
liu <liu@liudeMac-mini.local>
2025-02-20 21:11:36 +0800

    完成打包白屏问题

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,12 +2,12 @@ import 'tools-css/index.css'
 import 'tools-javascript'
 import 'tools-websocket'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia';
+
 import App from './App.vue'
 import './styles/index.less'
 import Config from './utils/Config'
 
-const pinia = createPinia();
+
 const app = createApp(App)
 //#ifvar-dev
 import ElementPlus from 'element-plus'
@@ -16,7 +16,7 @@ import { createTerminal } from 'vue-web-terminal'
 app.use(ElementPlus)
 // 全局注册 Terminal 组件
 app.use(createTerminal());
-app.use(pinia);
+
 //#endvar
 Config.init(app)
 
